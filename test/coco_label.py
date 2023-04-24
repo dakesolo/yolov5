8c46404e418ccd32a83c3dc6ff3d7e18f2aac198
dk.zhang <dk.zhang@d1m.cn>
2023-04-24 17:55:10 +0800

    grid anchor+

--- test/coco_label.py
+++ test/coco_label.py
@@ -1,12 +1,15 @@
 import os
 import sys
 
+import cv2
 import torch
-from torch import nn
+from matplotlib import pyplot as plt
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+print(BASE_DIR)
 sys.path.append(BASE_DIR)
 
+from pycocotools.coco import COCO
 from models.yolov5 import Grid, Anchor, box_ciou, Anchor2, Grid2
 import time
 
@@ -32,7 +35,7 @@ def test_grid2():
         [40, 40],
         [20, 20]
     ]
-    grid = Grid2(torch.Tensor([[34, 500], [78, 44], [10, 199], [180, 67]]), torch.Tensor([640, 640]), torch.Tensor(grids))
+    grid = Grid2(torch.tensor([[34, 500], [78, 44], [10, 199], [180, 67]]), torch.tensor([640, 640]), torch.tensor(grids))
     print(grid.get_grid_positives())
 
 def test_anchor():
@@ -54,16 +57,16 @@ def test_anchor2():
         [[353, 337], [539, 341], [443, 432]]  # P5/32
     ]
     wh = [68, 120]
-    anchor_object = Anchor2(torch.Tensor(wh), torch.Tensor(anchors), torch.Tensor([4]))
+    anchor_object = Anchor2(torch.tensor(wh), torch.tensor(anchors), torch.tensor([4]))
     print(anchor_object.get_anchor_positives())
 
 def test_iou():
-    iou = box_ciou(torch.Tensor([5, 20, 30, 40]), torch.Tensor([[[10, 20, 30, 40], [20, 40, 12, 55]], [[10, 20, 30, 40], [20, 40, 12, 55]]]))
+    iou = box_ciou(torch.tensor([5, 20, 30, 40]), torch.tensor([[[10, 20, 30, 40], [20, 40, 12, 55]], [[10, 20, 30, 40], [20, 40, 12, 55]]]))
     print(iou)
 
-    # tttt = torch.Tensor([[5, 20, 30, 40], [5, 20, 30, 40]])
+    # tttt = torch.tensor([[5, 20, 30, 40], [5, 20, 30, 40]])
     # iou = box_ciou(tttt.unsqueeze(1).unsqueeze(1),
-    #                torch.Tensor([
+    #                torch.tensor([
     #                    [[[10, 20, 30, 40], [20, 40, 12, 55], [20, 40, 12, 55]], [[10, 20, 30, 40], [20, 40, 12, 55], [20, 40, 12, 55]], [[10, 20, 30, 40], [20, 40, 12, 55], [20, 40, 12, 55]]],
     #                    [[[10, 20, 30, 40], [20, 40, 12, 55], [20, 40, 12, 55]], [[10, 20, 30, 40], [20, 40, 12, 55], [20, 40, 12, 55]], [[10, 20, 30, 40], [20, 40, 12, 55], [20, 40, 12, 55]]]
     #                ]))
@@ -72,7 +75,7 @@ def test_iou():
 def test_iou_each():
     # 比较耗性能
     for i in range(100):
-        iou = box_ciou(torch.Tensor([5, 20, 30, 40]), torch.Tensor([10, 20, 30, 40]))
+        iou = box_ciou(torch.tensor([5, 20, 30, 40]), torch.tensor([10, 20, 30, 40]))
         print(iou)
 
 def test_iou_shape():
@@ -90,25 +93,25 @@ def test_iou_shape():
     # return
     # print(anchor_grid)
 
-    for item in torch.Tensor(anchor_grid)[...,:4]:
-        iou = box_ciou(item, torch.Tensor([68, 120, 34, 89]))
+    for item in torch.tensor(anchor_grid)[...,:4]:
+        iou = box_ciou(item, torch.tensor([68, 120, 34, 89]))
         iou = torch.where(iou < 0, 0, iou)
         print(iou)
     # print(item)
-    # grid = torch.Tensor(grid_obj.get_grid_positives())
-    # anchor = torch.Tensor(anchor_obj.get_anchor_positives())
+    # grid = torch.tensor(grid_obj.get_grid_positives())
+    # anchor = torch.tensor(anchor_obj.get_anchor_positives())
 
-    # anchor_grid = torch.Tensor.
+    # anchor_grid = torch.tensor.
     #for item in grid.chunk(grid.shape[0]):
     #    _anchor = torch.cat((anchor, item.repeat(anchor.size(1), 1)), dim=-1)
     #    print(_anchor)
         # print(item[..., :2])
 
     # print(anchor)
-    # iou = box_ciou(torch.Tensor([5, 20, 30, 40]), torch.Tensor([10, 20, 30, 40]))
+    # iou = box_ciou(torch.tensor([5, 20, 30, 40]), torch.tensor([10, 20, 30, 40]))
     # print(iou)
 def test_cat_grid_anchor():
-    a = torch.Tensor([
+    a = torch.tensor([
         [[2, 3], [5, 6], [9, 9]],
         [[1, 4], [6, 4], [6, 1]],
         [[6, 2], [8, 2], [5, 1]]
@@ -116,7 +119,7 @@ def test_cat_grid_anchor():
     a = torch.split(a, 1, dim=0)
     # a1 = a[0].repeat(3, 1, 1)
     # print(a1)
-    b = torch.Tensor([
+    b = torch.tensor([
         [[45, 69], [77, 91], [88, 162]],
         [[123, 160], [167, 287], [444, 207]],
         [[123, 160], [167, 287], [444, 207]],
@@ -126,12 +129,12 @@ def test_cat_grid_anchor():
     c3 = torch.cat([b, a[2].repeat(3, 1, 1)], dim=2)
     print(torch.stack([c1, c2, c3], dim=2))
 
-if __name__ == '__main__':
-    # test_iou()
-    current_time1 = time.time()
+
+def get_iou():
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # 申明用GPU
 
     # 单张图片多个target
-    targets = [[34, 500, 50, 80], [78, 44, 90, 100], [10, 199, 40, 200], [180, 67, 72, 31]]
+    targets = [[34, 500, 50, 80], [78, 44, 90, 100], [10, 199, 40, 200], [180, 67, 72, 31], [180, 67, 72, 31]]
 
     # 获取 anchors
     anchors = [
@@ -139,7 +142,8 @@ if __name__ == '__main__':
         [[242, 160], [189, 287], [391, 207]],  # P4/16
         [[353, 337], [539, 341], [443, 432]]  # P5/32
     ]
-    anchor_object = Anchor2(torch.Tensor(targets)[..., 2:], torch.Tensor(anchors), torch.Tensor([4]))
+    anchor_object = Anchor2(torch.tensor(targets, device=device)[..., 2:], torch.tensor(anchors, device=device),
+                            torch.tensor([4], device=device))
     anchor_positives = anchor_object.get_anchor_positives()
 
     # 获取 grids
@@ -148,18 +152,61 @@ if __name__ == '__main__':
         [40, 40],
         [20, 20]
     ]
-    grid_object = Grid2(torch.Tensor(targets)[..., :2], torch.Tensor([640, 640]),
-                 torch.Tensor(grids))
+    grid_object = Grid2(torch.tensor(targets, device=device)[..., :2], torch.tensor([640, 640], device=device),
+                        torch.tensor(grids, device=device))
     grid_positives = grid_object.get_grid_positives()
     grid_positives.repeat(1, 1, 3)
 
     # [index_r, index_c, x, y, w, h]
     grid_anchor_positives = torch.cat([grid_positives.unsqueeze(2).repeat(1, 1, 3, 1), anchor_positives], dim=3)
 
-    # torch.Tensor(targets).unsqueeze(1).unsqueeze(1)维度必须和torch.Tensor(grid_anchor_positives[..., 2:])一样才行
-    iou = box_ciou(torch.Tensor(targets).unsqueeze(1).unsqueeze(1),
-                    torch.Tensor(grid_anchor_positives[..., 2:]))
-    print(iou)
+    # torch.tensor(targets).unsqueeze(1).unsqueeze(1)维度必须和torch.tensor(grid_anchor_positives[..., 2:])一样才行
+    iou = box_ciou(torch.tensor(targets, device=device).unsqueeze(1).unsqueeze(1),
+                   grid_anchor_positives[..., 2:])
+    iou.clamp_(min=0)
+    print(grid_anchor_positives)
+    return torch.cat([grid_anchor_positives, iou.unsqueeze(3)], dim=3)
 
+def img_view():
+    dataDir = '../data/cat'
+    dataType = 'all'
+    annFile = '{}/annotations/annotations_{}.json'.format(dataDir, dataType)
+
+    coco = COCO(annFile)
+    print(coco.dataset.keys())
+
+    # 类别信息
+    catIds = coco.getCatIds(catNms=['cat'])
+    catInfo = coco.loadCats(catIds)
+    print(f"catIds:{catIds}")
+    print(f"catcls:{catInfo}")
+
+    # 图像信息
+    imgIds = coco.getImgIds(catIds=catIds)
+    index = 5  # 随便选择一张图
+    imgInfo = coco.loadImgs(imgIds[index])[0]
+    print(f"imgIds:{imgIds}")
+    print(f"img:{imgInfo}")
+
+    # 标注信息
+    annIds = coco.getAnnIds(imgIds=imgInfo['id'], catIds=catIds, iscrowd=None)
+    annsInfo = coco.loadAnns(annIds)
+    print(f"annIds:{annIds}")
+    print(f"annsInfo:{annsInfo}")
+
+    # 显示图像
+    # i = io.imread(imgInfo['coco_url'])
+
+    i = cv2.imread(os.path.join(r'../data/cat/images', imgInfo['file_name']))
+    plt.imshow(i)
+    plt.axis('off')
+    coco.showAnns(annsInfo, True)
+    plt.show()
+    # plt.savefig('testbluelinew.jpg')
+
+if __name__ == '__main__':
+    current_time1 = time.time()
+    print(get_iou())
+    # img_view()
     current_time2 = time.time()
     print(current_time2 - current_time1)
